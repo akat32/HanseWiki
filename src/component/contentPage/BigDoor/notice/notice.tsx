7e575f4d64182364b767d7a534534271b3871bad
akat32 <baduk32127@gmail.com>
2019-04-12 06:38:54 +0900

    commit something

--- src/component/contentPage/BigDoor/notice/notice.tsx
+++ src/component/contentPage/BigDoor/notice/notice.tsx
@@ -1,10 +1,13 @@
 import React, { Component, useState, useEffect } from 'react'
 import axios from 'axios'
 import './notice.scss'
-
+ 
+import api from '../../../../api.json'
 interface NoticeProps {
     index: Object
 }
+
+const obj = <p>sdasdasd</p>
 let list = [
     { "index" : <span>환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다환영합니다!</span> },
     { "index" : <span>환영합니다!</span> },
@@ -12,11 +15,24 @@ let list = [
     { "index" : <span>환영합니다!</span> },
 ]
 export const Notice = ()=> {
-    useEffect;
+    const [data, setData] = useState()
+    async function axiosAPI(url: string) {
+        let result = await axios.post(url, { category: 1} )
+        let obj = result.data.obj;
+        setData(obj);
+        return result.data.obj;
+    }
+    useEffect( ()=> {
+        let url = api.url + "loadContent"
+        axiosAPI(url);
+    });
     return (
-        <ul className = "innerNotice">
-            { list.map ( i => (<NoticeList index={i.index}/>))}
-        </ul>
+        <>
+            {data}
+        </>
+        // <ul className = "innerNotice">
+        //     { list.map ( i => (<NoticeList index={i.index}/>))}
+        // </ul>
     )
 }
 const NoticeList = (props:NoticeProps)=> {
