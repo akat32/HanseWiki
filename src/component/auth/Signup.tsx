682d85dd5926393a47a6da0ff5051ce09e27a1af
akat32 <baduk32127@gmail.com>
2019-04-16 16:20:43 +0900

    add chkPerson & precautions

--- src/component/auth/Signup.tsx
+++ src/component/auth/Signup.tsx
@@ -21,12 +21,21 @@ export const Signup = ()=> {
                 <input type = "text" placeholder = "닉네임을 입력하세요"/>
                 <p>실명</p>
                 <input type = "text" placeholder = "실제 이름을 입력하세요"/>
+                <p>약관 동의</p>
+                <div className = "chkPerson">
+                    <p>휴먼 테스트</p>
+                    <input readOnly/>
+                    <p>답변</p>
+                    <input type="text"/>
+                </div>
+                <button className = "accept">가입</button>
             </div>
             <div className = "precautions">
-
-            </div>
-            <div className = "chkPerson">
-
+                <p><span>2000</span>명이 한세위키를 사용중입니다!</p>
+                <p><span>12</span>명의 관리자가 한세위키를 관리중입니다!</p>
+                <p>총<span>1000</span>개의 문서가 있습니다!</p>
+                <p>개발자는 보안 이외의 책임을 지지 않습니다.</p>
+                <p>실명을 왜 받냐구요? 범죄 일어날까봐 무서워서요..</p>
             </div>
         </div>
     )
